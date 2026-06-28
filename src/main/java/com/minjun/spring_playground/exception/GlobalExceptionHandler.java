baaf50abcced1d2d61ecc0eb1e5a3ce3bf64a1de
MinJun916 <ed125248@gmail.com>
2026-06-29 00:58:00 +0900

    feat: implement GlobalExceptionHandler to manage exceptions and provide consistent error responses across the application

---
+++ src/main/java/com/minjun/spring_playground/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.minjun.spring_playground.exception;
+
+import com.minjun.spring_playground.dto.ResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(ResponseStatusException.class)
+  public ResponseEntity<ResponseDTO<Void>> handleResponseStatusException(
+      ResponseStatusException exception) {
+
+    return ResponseEntity.status(exception.getStatusCode())
+        .body(new ResponseDTO<>(false, exception.getReason(), null));
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ResponseDTO<Void>> handleValidationException(
+      MethodArgumentNotValidException exception) {
+
+    String message = exception.getBindingResult().getFieldErrors().getFirst().getDefaultMessage();
+
+    return ResponseEntity.badRequest().body(new ResponseDTO<>(false, message, null));
+  }
+}
